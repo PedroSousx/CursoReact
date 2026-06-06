93073889be4bbb59d5f26acdf37f7b49468ac5ba
Pedro <pedroh.scintra@gmail.com>
2026-06-06 16:04:42 -0300

    feat: learn .map

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
+import { useState } from "react";
+
 export function App() {
+  const [value, setValue] = useState("");
+  const [list, setList] = useState([
+    { id: 1, text: "Fazer Café" },
+    { id: 2, text: "Fazer Almoço" },
+    { id: 3, text: "Fazer Jantar" },
+  ]);
 
   return (
-    <>
-    <h1>Hello, world</h1>
-    </> 
+    <div>
+      <input type="text" 
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+        <button onClick={() => (setList([
+          ...list,
+          { id: list.length + 1, text: value }
+        ]))}>
+          Adicionar
+      </button>
+      <ol>
+        {list.map((listItem) => (
+          <li key={listItem.id}>{listItem.text}</li>
+        ))}
+      </ol>
+    </div>
   );
 }
 
-export default App
+export default App;
